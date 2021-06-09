b8a11175a9d759615200ae6697c9fb59e71bac0c
Liviu Voica <voica.liviu10@gmail.com>
2021-06-09 07:05:37 +0300

    Forgot password design, layout and full translation

--- resources/js/pages/auth/password/email.vue
+++ resources/js/pages/auth/password/email.vue
@@ -1,29 +1,42 @@
 <template>
   <div class="row">
-    <div class="col-lg-8 m-auto">
-      <card :title="$t('reset_password')">
-        <form @submit.prevent="send" @keydown="form.onKeydown($event)">
-          <alert-success :form="form" :message="status" />
+    <div class="col-lg-7 m-auto container-login-form">
+      <form @submit.prevent="send" @keydown="form.onKeydown($event)">
+        <alert-success :form="form" :message="status" />
+        <!-- HEADER SECTION START -->
+        <div class="form-group row">
+          <div class="col-md-12 container-login-form-header">
+            <h1 class="text-center">
+              {{ $t('login_system.reset_password_form.form_title') }}
+            </h1>
+          </div>
+        </div>
+        <!-- HEADER SECTION END -->
 
-          <!-- Email -->
+        <div class="container-login-form-body">
+          <!-- EMAIL CONTAINER SECTION START -->
           <div class="form-group row">
-            <label class="col-md-3 col-form-label text-md-right">{{ $t('email') }}</label>
-            <div class="col-md-7">
-              <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" type="email" name="email">
+            <label class="col-md-12 col-form-label">{{ $t('login_system.reset_password_form.email_label') }}</label>
+            <div class="col-md-12">
+              <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" type="email" name="email" :placeholder="$t('login_system.reset_password_form.email_placeholder')">
               <has-error :form="form" field="email" />
             </div>
           </div>
+          <!-- EMAIL CONTAINER SECTION END -->
+        </div>
 
-          <!-- Submit Button -->
+        <div class="container-login-form-footer">
+          <!-- SUBMIT BUTTON SECTION START -->
           <div class="form-group row">
-            <div class="col-md-9 ml-md-auto">
+            <div class="col-md-12 d-flex justify-content-around m-auto">
               <v-button :loading="form.busy">
-                {{ $t('send_password_reset_link') }}
+                {{ $t('login_system.reset_password_form.submit_button') }}
               </v-button>
             </div>
           </div>
-        </form>
-      </card>
+          <!-- SUBMIT BUTTON SECTION END -->
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -35,7 +48,7 @@ export default {
   middleware: 'guest',
 
   metaInfo () {
-    return { title: this.$t('reset_password') }
+    return { title: this.$t('login_system.reset_password_form.page_title') }
   },
 
   data: () => ({
@@ -56,3 +69,31 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+  .container-login-form {
+    border: 1px solid #6c757d;
+    border-radius: 5px;
+    &-header {
+      margin: 20px 0px !important;
+      & h1 {
+        margin-bottom: 0px !important;
+        user-select: none;
+      }
+    }
+    &-body {
+      margin: 40px 0px !important;
+      & label {
+        font-size: 18px;
+        font-weight: bold;
+      }
+    }
+    &-footer {
+      margin: 40px 0px !important;
+      & .btn {
+        margin: 0px 10px;
+        width: 200px;
+        border-radius: 0px;
+      }
+    }
+  }
+</style>
