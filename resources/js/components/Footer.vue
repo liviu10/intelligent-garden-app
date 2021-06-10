f5bd1a5ebf975ae96502546ffbf2a035ac118f95
Liviu Voica <voica.liviu10@gmail.com>
2021-06-10 07:23:45 +0300

    Footer layout, design, translation and registering the component

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+  <footer class="footer-content">
+    <p class="footer-content-paragraph">
+      {{ $t('footer_section.first_paragraph.title') }}
+      <a href="https://getbootstrap.com/docs/4.6/content/" :title="$t('footer_section.first_paragraph.bootstrap_tooltip')" taget="_blank">Bootstrap v4.6</a>,
+      <a href="https://vuejs.org/v2/guide/" :title="$t('footer_section.first_paragraph.vuejs_tooltip')" taget="_blank">VueJs v2</a> and
+      <a href="https://laravel.com/docs/8.x/releases" :title="$t('footer_section.first_paragraph.laravel_tooltip')" taget="_blank">Laravel v8</a>
+    </p>
+    <p class="footer-content-paragraph">
+      <span>{{ $t('footer_section.second_paragraph.title') }}</span>
+      <a href="https://github.com/liviu10" :title="$t('footer_section.second_paragraph.github_tooltip')">Github</a> |
+      <a href="https://www.linkedin.com/in/liviu-voica-849732a4/" :title="$t('footer_section.second_paragraph.linkedin_tooltip')">Linkedin</a>
+    </p>
+    <p class="footer-content-paragraph">
+      <span>Copyright ©</span>
+      <a href="https://liviu-voica.ro/">Liviu Voica</a>
+      2021
+    </p>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style lang="scss" scoped>
+    .footer-content {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        margin: 48px 0px;
+        &-paragraph {
+            margin-bottom: 0px !important;
+            user-select: none;
+            & span {
+                font-weight: bold;
+            }
+        }
+        &-paragraph:first-of-type {
+            font-size: 20px;
+        }
+    }
+</style>
