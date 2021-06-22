ac4cb275d7e9730f95394d733847bfdb9e41c9b1
Liviu Voica <voica.liviu10@gmail.com>
2021-06-22 05:44:53 +0300

    Documentation improvements: - added page translations in english, spanish, french, italian and romanian; - created the Documentation raw page;

---
+++ resources/js/pages/documents/documentation.vue
@@ -0,0 +1,63 @@
+/* eslint-disable vue/attribute-hyphenation */
+/* eslint-disable vue/attribute-hyphenation */
+<template>
+  <div class="documentation">
+    <div class="documentation--header">
+      <h1 class="display-6">
+        {{ $t('documentation_page.page_title') }}
+      </h1>
+    </div>
+    <div class="documentation--paragraph">
+      <p class="mb-0 text-center">
+        {{ $t('documentation_page.first_paragraph') }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  middleware: 'auth',
+  metaInfo () {
+    return { title: this.$t('documentation_page.page_title') }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.documentation {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column !important;
+  flex-wrap: wrap !important;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  @media only screen and (max-width: 576px) {
+    padding-top: 0px;
+    padding-bottom: 0px;
+  }
+  &--header {
+    display: flex !important;
+    justify-content: center !important;
+    align-items: center !important;
+    & .display-6 {
+      display: flex !important;
+      justify-content: center !important;
+      align-items: center !important;
+      margin-bottom: 0px;
+      height: 60px;
+      text-align: center !important;
+    }
+  }
+  &--paragraph {
+    display: flex !important;
+    justify-content: center !important;
+    align-items: center !important;
+    flex-direction: column !important;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
+}
+</style>
