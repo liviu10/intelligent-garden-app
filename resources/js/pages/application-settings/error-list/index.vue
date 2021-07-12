dd390514be6e1c4c26265736de583164fe07e517
Liviu Voica <voica.liviu10@gmail.com>
2021-07-12 22:04:24 +0300

    Settings improvements: - translated the cards from Application Settings Page and the subpages of route: "application-settings/page_name" in Romanian, French, English, Italian and Spanish; - imported new font awesome icons in this project; - created routes for all the configuration settings that starts from the Application Settings page;

---
+++ resources/js/pages/application-settings/error-list/index.vue
@@ -0,0 +1,71 @@
+/* eslint-disable vue/attribute-hyphenation */
+/* eslint-disable vue/attribute-hyphenation */
+<template>
+  <div class="application-settings-error-list">
+    <div class="application-settings-error-list--header">
+      <h1 class="display-6">
+        {{ $t('application_settings_page.error_list.page_title') }}
+      </h1>
+    </div>
+    <div class="application-settings-error-list--subheader">
+      <router-link to="/application-settings" class="btn btn-success">
+        <fa icon="arrow-left" fixed-width />
+        {{ $t('application_settings_page.go_back_button') }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  middleware: 'auth',
+  metaInfo () {
+    return { title: this.$t('application_settings_page.error_list.page_title') }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.application-settings-error-list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column !important;
+  flex-wrap: wrap !important;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  @media only screen and (max-width: 576px) {
+    padding-top: 0px;
+    padding-bottom: 0px;
+  }
+  &--header {
+    display: flex !important;
+    justify-content: center !important;
+    align-items: center !important;
+    & .display-6 {
+      display: flex !important;
+      justify-content: center !important;
+      align-items: center !important;
+      margin-bottom: 0px;
+      height: 60px;
+      text-align: center !important;
+    }
+  }
+  &--subheader {
+    display: flex !important;
+    justify-content: flex-start !important;
+    align-items: center !important;
+    width: 100%;
+    margin: 1rem 0rem;
+  }
+  &--paragraph {
+    display: flex !important;
+    justify-content: center !important;
+    align-items: center !important;
+    flex-direction: column !important;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
+}
+</style>
--- resources/js/plugins/fontawesome.js
+++ resources/js/plugins/fontawesome.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // import { } from '@fortawesome/free-regular-svg-icons'
 
 import {
-  faUser, faLock, faSignOutAlt, faCog, faBookOpen
+  faUser, faLock, faSignOutAlt, faCog, faBookOpen, faArrowLeft, faFileExport
 } from '@fortawesome/free-solid-svg-icons'
 
 import {
@@ -13,7 +13,7 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 
 library.add(
-  faUser, faLock, faSignOutAlt, faCog, faGithub, faBookOpen
+  faUser, faLock, faSignOutAlt, faCog, faGithub, faBookOpen, faArrowLeft, faFileExport
 )
 
 Vue.component('Fa', FontAwesomeIcon)
